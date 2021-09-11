4df1438aa66de7526df954c7e7effbaac933d855
Kazuki_Kamizuru <kamizuru@k2ss.info>
2021-09-12 00:53:56 +0900

    feat: add iam role and policy to pulumi configuration

--- infra/__main__.py
+++ infra/__main__.py
@@ -1,18 +1,56 @@
-"""An AWS Python Pulumi program"""
+'''An AWS Python Pulumi program'''
+
+import json
 
 import pulumi
 import pulumi_aws as aws
 
-bot_messages = aws.dynamodb.Table("bot_messages",
+bot_messages = aws.dynamodb.Table('bot_messages',
                                   attributes=[aws.dynamodb.TableAttributeArgs(
-                                      name="key",
-                                      type="S",
+                                      name='key',
+                                      type='S',
                                   )],
-                                  billing_mode="PROVISIONED",
-                                  hash_key="key",
-                                  name="bot_messages",
+                                  billing_mode='PROVISIONED',
+                                  hash_key='key',
+                                  name='bot_messages',
                                   read_capacity=5,
                                   write_capacity=5,
                                   opts=pulumi.ResourceOptions(protect=True))
 
 pulumi.export('bot_messages', bot_messages.id)
+
+policy = aws.iam.Policy('TwitterBotPolicy',
+                        description='Twitter Bot Policy',
+                        policy=json.dumps({
+                            'Version': '2012-10-17',
+                            'Statement': [
+                                {
+                                    'Effect': 'Allow',
+                                    'Action': [
+                                        'ssm:GetParameter',
+                                        'sts:AssumeRole',
+                                        'dynamodb:GetItem',
+                                    ],
+                                    'Resource': '*'
+                                },
+                            ],
+                        }))
+
+role = aws.iam.Role('TwitterBotRole',
+                    path='/service-role/',
+                    assume_role_policy=json.dumps({
+                        'Version': '2012-10-17',
+                        'Statement': [
+                            {
+                                'Effect': 'Allow',
+                                'Action': 'sts:AssumeRole',
+                                'Principal': {
+                                    'Service': 'lambda.amazonaws.com'
+                                },
+                            },
+                        ],
+                    }))
+
+aws.iam.RolePolicyAttachment('TwitterBotRolePolicyAttachment',
+                             role=role,
+                             policy_arn=policy.arn)
